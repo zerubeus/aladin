a9df760e74b1f6fa84ba0d57488fe7bf50d25e78
zerubeus <zerubeus@gmail.com>
2025-02-13 11:21:19 +0100

    feat: replace IntelliJ Platform Gradle Plugin version with alias in build configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,6 @@ import org.jetbrains.intellij.platform.gradle.TestFrameworkType
 
 plugins {
     id("java") // Java support
-    id("org.jetbrains.intellij.platform") version "2.2.1" // IntelliJ Platform Gradle Plugin
     alias(libs.plugins.kotlin) // Kotlin support
     alias(libs.plugins.intelliJPlatform) // IntelliJ Platform Gradle Plugin
     alias(libs.plugins.changelog) // Gradle Changelog Plugin
